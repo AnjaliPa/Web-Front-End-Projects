1c8edce39d86ebe0de492d09828cc796ae504226
AnjaliPa <anjalipathak06@gmail.com>
2016-09-03 19:21:12 -0700

    All projects link and name that is uploded on heroku serever

---
+++ herokulinkforgithub/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+</style>
+</head>
+
+<body>
+     <h1>AnjaliPa/Front-End-Web-Developer-Nanodegree Links</h1>
+    <table  width="400px" height="180px"  >
+   <tr>
+       <td><a href="http://resumehtml.herokuapp.com/">Project Online Resume</a></td></tr>
+        
+         <tr>
+       <td><a href="http://project-neighborhood-map.herokuapp.com/">Project Neighborhood Map</a></td></tr>
+        
+         <tr>
+       <td><a href="http://project-mockup-to-article.herokuapp.com/">Project Mockup to Article</a></td></tr>
+        <tr>
+       <td><a href="http://project-classic-arcade-game.herokuapp.com/"> Project Classic Arcade Game Clone</a></td></tr>
+        
+         <tr>
+       <td><a href="http://project-build-a-portfolio-site.herokuapp.com/"> Project Build a Portfolio Site</a></td></tr>
+        
+         <tr>
+       <td><a href="http://project-animal-trading-cards.herokuapp.com/"> Project Animal Trading Cards</a></td></tr>
+       
+    </table>
+    
+    
+    <h1>AnjaliPa/Web-Front-End-Projects Links</h1>
+
+<table  width="400px" height="180px"  >
+   <tr>
+       <td><a href="https://desolate-lowlands-52477.herokuapp.com/">Bouncing ball</a></td></tr>
+    <tr><td>
+        <a href="http://infinite-escarpment-79278.herokuapp.com/">Drag and Drop</a></td></tr>
+     <tr><td>
+        <a href="http://safe-mountain-31906.herokuapp.com/">Simple project</a></td></tr>
+    
+     <tr><td>
+        <a href="http://young-fjord-68103.herokuapp.com/">WebSocket Connection with server</a></td></tr>
+    
+     <tr><td>
+        <a href="http://frozen-woodland-42838.herokuapp.com/">Calculate Distance</a></td></tr>
+    
+    <tr><td>
+        <a href="http://pacific-harbor-46138.herokuapp.com/">WebSQL Example</a></td></tr>
+    
+        <tr><td>
+            <a href="http://polar-waters-91761.herokuapp.com/">Calculator</a></td></tr>
+    
+
+ <tr><td>
+            <a href="http://socketappp.herokuapp.com/">BroadChat</a></td></tr>
+    
+<tr><td>
+            <a href="http://rtceasy.herokuapp.com/">RTC Server</a></td></tr>
+
+
+     
+
+  
+</table>
+   
+</body>
+</html>
